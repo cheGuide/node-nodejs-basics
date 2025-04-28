754466e335b910fbf5fe3c1af6ff55456293f1a0
cheGuide <danilanemtsov@gmail.com>
2025-04-29 00:27:45 +0300

    rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import fs from "node:fs/promises"
+import path from 'path';
+
+const __dirname = import.meta.dirname;
+
+const FILE_PATH = path.join(__dirname, "./files/wrongFilename.txt");
+const NEW_FILE_NAME = path.join(__dirname, "./files/properFilename.md");
+
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        const isFileExists = await fs.stat(NEW_FILE_NAME).then(() => true).catch(() => false);
+
+        if (isFileExists) {
+            throw new Error('File already exists');
+        }
+        
+        await fs.rename(FILE_PATH, NEW_FILE_NAME)
+    } catch (error) {
+        // error if 
+        // file wrongFilename.txt don't exists
+        // properFilename.md file alreasy exists  
+        throw new Error("FS operation failed");
+    }
 };
 
 await rename();
